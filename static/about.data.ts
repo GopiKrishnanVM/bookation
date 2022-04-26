13360ea5adcc40d6514ddccb7f579abc3693b5a8
muhil-kpk <kaarmuhilankp@gmail.com>
2022-04-26 18:37:55 +0530

    about data

--- static/about.data.ts
+++ static/about.data.ts
@@ -1,19 +1,19 @@
 /* This example requires Tailwind CSS v2.0+ */
 export const people = [
   {
-    name: "Michael Foster",
+    name: "Kaarmuhilan KP",
     role: "Co-Founder / CTO",
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
   },
   {
-    name: "Michael Foster",
+    name: "Praveen Kumar P",
     role: "Co-Founder / CTO",
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
   },
   {
-    name: "Michael Foster",
+    name: "Gopi Krishnan VM",
     role: "Co-Founder / CTO",
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
@@ -23,28 +23,24 @@ export const people = [
 /* This example requires Tailwind CSS v2.0+ */
 export const salesTeam = [
   {
-    name: "Michael Foster",
+    name: "Praveen Kumar",
     role: "Co-Founder / CTO",
     contactNumber: "+91 6383172402",
-    contactEmail : "bookation.offical@gmail.com",
+    contactEmail: "bookation.offical@gmail.com",
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
   },
-  {
-    name: "Michael Foster",
-    role: "Co-Founder / CTO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
+  // {
+  //   name: "Kaarmuhilan KP",
+  //   role: "Co-Founder / CTO",
+  //   contactNumber: "+91 6383172402",
+  //   contactEmail: "bookation.offical@gmail.com",
+  //   imageUrl:
+  //     "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+  // },
 ];
 
 export const techTeam = [
-  {
-    name: "Michael Foster",
-    role: "Co-Founder / CTO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  },
   {
     name: "Michael Foster",
     role: "Co-Founder / CTO",
