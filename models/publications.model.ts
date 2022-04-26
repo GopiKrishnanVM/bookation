c7be94ec432e2e644f201b43b53e4bd17af34bee
GopiKrishnanVM <85819439+GopiKrishnanVM@users.noreply.github.com>
2022-04-26 22:09:32 +0530

    publications model

--- models/publications.model.ts
+++ models/publications.model.ts
@@ -5,5 +5,15 @@ export interface IPublicationsModel extends IBaseIdModel {
   name: string;
   price: string;
   author: IAuthor;
-  image: string;
+  imageSrc: string;
+  imageAlt: string;
+  slug: string;
+
+  description: string;
+  providers: Array<IBookProviders>;
+}
+export interface IBookProviders {
+  name: string;
+  description: string;
+  links: Array<{ title: string; link: string }>;
 }
