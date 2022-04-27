ae1c4daf0d7040759a52c8df140d5e1b3f1da7e6
GopiKrishnanVM <85819439+GopiKrishnanVM@users.noreply.github.com>
2022-04-27 20:06:53 +0530

    head component next link

--- components/layout/header.component.tsx
+++ components/layout/header.component.tsx
@@ -23,13 +23,15 @@ const HeaderComponent: React.FC<Props> = ({ setOpen }) => {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="h-16 flex items-center justify-between">
               <div className="flex justify-start lg:w-0 lg:flex-1">
-                <a href="/">
-                  <img
-                    className="h-8 w-auto sm:h-10"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt=""
-                  />
-                </a>
+                <NextLink href="/">
+                  <a>
+                    <img
+                      className="h-8 w-auto sm:h-10"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                      alt=""
+                    />
+                  </a>
+                </NextLink>
               </div>
               <div className="hidden h-full lg:flex">
                 {/* Flyout menus */}
